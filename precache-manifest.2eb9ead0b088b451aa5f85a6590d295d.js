1d36162298ba2b3bc61736c9223466f2378cb476
saif <mujawarmsaif@gmail.com>
2019-11-27 17:48:06 +0530

    Updates

---
+++ precache-manifest.2eb9ead0b088b451aa5f85a6590d295d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037995964095c87d9bb2a8be391a4344",
+    "url": "http//:saaiif.github.io/WatchCardModule/index.html"
+  },
+  {
+    "revision": "7bcdad126e21e932a93f",
+    "url": "http//:saaiif.github.io/WatchCardModule/static/css/main.0081b9a6.chunk.css"
+  },
+  {
+    "revision": "2c3827e842356f1e93d3",
+    "url": "http//:saaiif.github.io/WatchCardModule/static/js/2.5ba8213c.chunk.js"
+  },
+  {
+    "revision": "7bcdad126e21e932a93f",
+    "url": "http//:saaiif.github.io/WatchCardModule/static/js/main.f372c29f.chunk.js"
+  },
+  {
+    "revision": "1572b7712e98ced74339",
+    "url": "http//:saaiif.github.io/WatchCardModule/static/js/runtime-main.ed8d4925.js"
+  }
+]);
